b7eff378f2ad569e72cf9070ca51024a8fd68d95
Andrzej <andrzej.bilant@gmail.com>
2019-11-05 10:41:59 +0100

    dodanie plików

--- src/RubiksCube.java
+++ src/RubiksCube.java
@@ -1,32 +1,28 @@
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.TimerTask;
-import java.util.concurrent.atomic.AtomicInteger;
+
+import javafx.application.Platform;
+
+
+import java.util.*;
+
 import javafx.application.Application;
 import javafx.geometry.Point3D;
 import javafx.scene.*;
-import javafx.scene.effect.Light;
-import javafx.scene.effect.Lighting;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.*;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.*;
-import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Rotate;
-import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 import javafx.animation.RotateTransition;
 import javafx.util.Duration;
 
-import javax.sound.sampled.*;
 
 /**
  * Simple implementation of the Rubik's cube using JavaFX 3D
@@ -39,9 +35,13 @@ public class RubiksCube extends Application {
     private double mousePosY;
     private double mouseOldX;
     private double mouseOldY;
+    private int rozmiar_wyswietlacza=9;
     private int przekretlo_angle = 0;
     private boolean flaga = false;
-    //Hallo Hallo w morde dajo
+    public Text text;
+    public int aktualnyCzas=0;
+    public Timer timer=new Timer();
+    private boolean gotowe=false;
 
     @Override
     public void start(Stage primaryStage) {
@@ -52,9 +52,7 @@ public class RubiksCube extends Application {
         light.setTranslateZ(-400);
 
         //swiatlo w srodku mikrofali
-        //TODO: to swiatlo jest teoretycznie w mikrofali i dziala, plan jet taki zeby sie wlaczalo podczas wlaczenie animacji
-        //zeby ustawic najlatwiej wylaczyc swiatlo 'light' i zostawic tylko to
-        PointLight mikrofalowe = new PointLight(Color.rgb( 0, 0 ,0));
+        PointLight mikrofalowe = new PointLight(Color.rgb(0, 0, 0));
         mikrofalowe.setTranslateX(350);
         mikrofalowe.setTranslateY(300);
         mikrofalowe.setTranslateZ(300);
@@ -62,115 +60,116 @@ public class RubiksCube extends Application {
         //calosc 1050 650 1000 0 0 500
         //Robienie klockow do mikrofali
         //top 1050 50 1000 spawn 0 0 500
-        Box top = new Box(1050,50,1000);
+        Box top = new Box(1050, 50, 1000);
         top.setTranslateX(0);
         top.setTranslateY(0);
         top.setTranslateZ(500);
         PhongMaterial phong = new PhongMaterial();
-        phong.setDiffuseColor(Color.ORANGE);
+        phong.setDiffuseColor(Color.SILVER);
         top.setMaterial(phong);
 
         //bot 1050 50 1000 spawn 0 600 500
-        Box bot = new Box(1050,50,1000);
+        Box bot = new Box(1050, 50, 1000);
         bot.setTranslateX(0);
         bot.setTranslateY(600);
         bot.setTranslateZ(500);
         PhongMaterial phong2 = new PhongMaterial();
-        phong2.setDiffuseColor(Color.WHEAT);
+        phong2.setDiffuseColor(Color.SILVER);
         bot.setMaterial(phong2);
 
         //left 650 50 1000 spawn 0 0
-        Box left = new Box(50,650,1000);
+        Box left = new Box(50, 650, 1000);
         left.setTranslateX(-500);
         left.setTranslateY(300);
         left.setTranslateZ(500);
         PhongMaterial phong3 = new PhongMaterial();
-        phong3.setDiffuseColor(Color.SKYBLUE);
+        phong3.setDiffuseColor(Color.SILVER);
         left.setMaterial(phong3);
 
         //right 650 50 1000 spawn 1000 0
-        Box right = new Box(50,650,1000);
+        Box right = new Box(50, 650, 1000);
         right.setTranslateX(500);
         right.setTranslateY(300);
         right.setTranslateZ(500);
         PhongMaterial phong4 = new PhongMaterial();
-        phong4.setDiffuseColor(Color.OLIVE);
+        phong4.setDiffuseColor(Color.SILVER);
         right.setMaterial(phong4);
 
         //back 1050 650 1000
-        Box back = new Box(1050,650,50);
+        Box back = new Box(1050, 650, 50);
         back.setTranslateX(0);
         back.setTranslateY(300);
         back.setTranslateZ(1000);
         PhongMaterial phong5 = new PhongMaterial();
-        phong5.setDiffuseColor(Color.BROWN);
+        phong5.setDiffuseColor(Color.SILVER);
         back.setMaterial(phong5);
 
         //front 1050 650 1000
-        Box front = new Box(300,650,1025);
+        Box front = new Box(300, 650, 1025);
         front.setTranslateX(350);
         front.setTranslateY(300);
         front.setTranslateZ(500);
         PhongMaterial phong6 = new PhongMaterial();
-        phong6.setDiffuseColor(Color.VIOLET);
-        front.setMaterial(phong6);
+        phong5.setDiffuseColor(Color.SILVER);
+        front.setMaterial(phong5);
 
-        //drzwiczki 750 650 5
-        //TODO: zrobic to na przezroczyste tak zeby imitowalo drzwiczki
-        Box drzwiczki = new Box(750,650,5);
+
+        Box drzwiczki = new Box(750, 650, 5);
         drzwiczki.setTranslateX(-100);
         drzwiczki.setTranslateY(300);
         PhongMaterial phong8 = new PhongMaterial();
         //phong8.setDiffuseColor(Color.SILVER);
-        phong8.setDiffuseColor(new Color(1,1,1,0.6));  // Note alpha of 0.6
+        phong8.setDiffuseColor(new Color(1, 1, 1, 0.6));  // Note alpha of 0.6
         phong8.diffuseMapProperty();
         drzwiczki.setMaterial(phong8);
 
-        //wyswietlacz
-        Box  wyswietlacz = new Box(200,100,5);
-        wyswietlacz.setTranslateX(350);
-        wyswietlacz.setTranslateY(100);
-        wyswietlacz.setTranslateZ(-15);
-        PhongMaterial phong9 = new PhongMaterial();
-        phong9.setDiffuseColor(Color.BLACK);  // Note alpha of 0.6
-        wyswietlacz.setMaterial(phong9);
-
         //przkretlo
-        Cylinder cylinder = new Cylinder(100,100);
+        Cylinder cylinder = new Cylinder(100, 100);
         cylinder.setTranslateX(350);
         cylinder.setTranslateY(400);
         cylinder.setTranslateZ(-50);
-        cylinder.setRotationAxis(new Point3D(1,0,0));
+        cylinder.setRotationAxis(new Point3D(1, 0, 0));
         cylinder.setRotate(90);
+
         PhongMaterial material = new PhongMaterial();
-        material.setDiffuseMap(new Image(getClass().getResourceAsStream("metal-texture.jpg")));
-        cylinder.setMaterial(material);
+        material.setDiffuseMap(new Image(getClass().getResourceAsStream("metal.jpg")));
+
+
+        PhongMaterial materiall = new PhongMaterial();
+        materiall.setDiffuseMap(new Image(getClass().getResourceAsStream("met.jpg")));
+
+        cylinder.setMaterial(materiall);
 
         //krecacy sie spodek w mikrofali
-        Cylinder spodek = new Cylinder(300,10);
+        Cylinder spodek = new Cylinder(300, 10);
         spodek.setTranslateX(-150);
         spodek.setTranslateY(550);
         spodek.setTranslateZ(500);
-        spodek.setMaterial(material);
-
+        spodek.setMaterial(materiall);
 
         //przycisk
-        Cylinder button = new Cylinder(10,30);
+        Cylinder button = new Cylinder(20, 30);
         button.setTranslateX(360);
         button.setTranslateY(200);
-        //button.setTranslateZ(0);
-        button.setRotationAxis(new Point3D(1,0,0));
+        button.setTranslateZ(-20);
+        button.setRotationAxis(new Point3D(1, 0, 0));
         button.setRotate(90);
         PhongMaterial phong7 = new PhongMaterial();
         phong7.setDiffuseColor(Color.LIMEGREEN);
         button.setMaterial(phong7);
 
-        //SRODEK
-        Sphere kulka = new Sphere(10,10);
-
-
+        //wyświetlacz
+        text = new Text();
+        text.setText(" 0:00 ");
+        text.setStyle("-fx-font-size: 60;");
+        text.setCache(true);
+        BorderPane borderPane = new BorderPane();
+        borderPane.setTranslateZ(-20);
+        borderPane.setTranslateX(290);
+        borderPane.setStyle("-fx-border-color: black;-fx-background-color: #66CCFF;");
+        borderPane.setTop(text);
         //gdybys cos dodawal to tutaj musisz wpisac wszystkie obiekty 3D + swiatla
-        Group group = new Group( spodek, kulka, wyswietlacz, drzwiczki, button , top, left, right, bot, back, front, cylinder, light, mikrofalowe);
+        Group group = new Group(borderPane,spodek,drzwiczki, button, top, left, right, bot, back, front, cylinder, light, mikrofalowe);
 
 
         Scene scene = new Scene(
@@ -182,7 +181,6 @@ public class RubiksCube extends Application {
 
         scene.setFill(Color.rgb(10, 10, 40));
 
-        //TODO: moze to kamera sie rusza podczas rotatetransition, jak cos to jest tutaj v
         PerspectiveCamera camera = new PerspectiveCamera();
         camera.setTranslateZ(-2000);
         scene.setCamera(camera);
@@ -192,117 +190,168 @@ public class RubiksCube extends Application {
 
         Rotate rotateX = new Rotate(30, 0, 0, 0, Rotate.X_AXIS);
         Rotate rotateY = new Rotate(20, 0, 0, 0, Rotate.Y_AXIS);
-        //cylinder.setRotationAxis(new Point3D(0,0,1));
 
-       group.getTransforms().addAll(rotateX,rotateY);
-
+        group.getTransforms().addAll(rotateX, rotateY);
 
 
         scene.setOnMousePressed(me -> {
-            if(mouseOldX == me.getSceneX() && mouseOldY == me.getSceneY())
+            if (!flaga&&przekretlo_angle>0&&!gotowe) //flaga sprawdza czy przycisk jest wcisniety
             {
-                //RotateTransition rt0 = new RotateTransition(Duration.millis(przekretlo_angle*1000),cylinder);
-                RotateTransition rt1 = new RotateTransition(Duration.millis(przekretlo_angle*1000),spodek);
-                //rt0.setAxis(new Point3D(0,0,1));
-                //rt0.setByAngle(przekretlo_angle);
-                rt1.setAxis(new Point3D(0,1,0));
-                rt1.setByAngle(przekretlo_angle*60);
+                RotateTransition rt1 = new RotateTransition(Duration.millis(przekretlo_angle * 1000), spodek);
+                rt1.setAxis(new Point3D(0, 1, 0));
+                rt1.setByAngle(przekretlo_angle * 60);
+
+                //zmiana koloru
+                phong7.setDiffuseColor(Color.RED);
+                button.setMaterial(phong7);
+                flaga = true;
+                mikrofalowe.setColor(Color.SALMON);
+                //przygotowanie muzyki
+                String musicFile = "C:\\Users\\Andrzej\\IdeaProjects\\Projekt_Kck2\\src\\mmm.mp3";
+                Media sound = new Media(new File(musicFile).toURI().toString());
+                MediaPlayer mediaPlayer = new MediaPlayer(sound);
+                Media end = new Media(new File("C:\\Users\\Andrzej\\IdeaProjects\\Projekt_Kck2\\src\\beep.mp3").toURI().toString());
+                MediaPlayer finalMediaPlayer = new MediaPlayer(end);
+                finalMediaPlayer.setStartTime(new Duration(1200));
+                finalMediaPlayer.setStopTime(new Duration(3050));
+
+                Thread runnable = new Thread() {
+                    @Override
+                    public void run() {
+                        aktualnyCzas = przekretlo_angle;
+                        TimerTask update = new TimerTask() {
+                            @Override
+                            public void run() {
+                                text.setText(print(aktualnyCzas));
+                            }
+                        };
+                        TimerTask x = new TimerTask() {
+                            @Override
+                            public void run() {
+
+                                Platform.runLater(update);
+                                aktualnyCzas--;
+                            }
+                        };
+
+                        timer.schedule(x, 1000, 1000);
+                        mediaPlayer.play();
 
 
-                if (!flaga) //flaga sprawdza czy przycisk jest wcisniety
-                {
-                    //zmiana koloru
-                    phong7.setDiffuseColor(Color.RED);
-                    button.setMaterial(phong7);
-                    flaga = true;
+                        while (aktualnyCzas > 0) {
+                            try {
+                                Thread.sleep(50);
+                            } catch (InterruptedException ie) {
+                                ie.printStackTrace();
+                            }
 
-                    //przygotowanie muzyki
-                    String musicFile = "C:\\Users\\Adam\\IdeaProjects\\Projekt_Kck\\src\\mmm.mp3";
-                    Media sound = new Media(new File(musicFile).toURI().toString());
-                    MediaPlayer mediaPlayer = new MediaPlayer(sound);
-                    mediaPlayer.play();
-
-                    //wlaczenie rotatetransition
-                    mikrofalowe.setColor(Color.SALMON);
-                    rt1.play();
-                    MediaPlayer finalMediaPlayer = mediaPlayer;
-
-                    //na koniec wykonywania rotate transition wroc przekretlo do stanu poczatkowego + wylacz muzyke
-                    rt1.setOnFinished(ae -> {
-                        for (int i = 0; i < przekretlo_angle && przekretlo_angle >= 0; i ++)
-                        {
-                            Rotate rt0 = new Rotate(-10, 0, 0, 0, Rotate.Y_AXIS);
-                            cylinder.getTransforms().add(rt0);
-                            przekretlo_angle = przekretlo_angle - 1;
                         }
-                        finalMediaPlayer.stop();
+                        timer.cancel();
+                        timer = new Timer();
+                        text.setText(" 0:00 ");
+                        gotowe = true;
                         phong7.setDiffuseColor(Color.LIMEGREEN);
                         button.setMaterial(phong7);
+                        przekretlo_angle = 0;
+                        rt1.pause();
+                        gotowe = false;
                         mikrofalowe.setColor(Color.BLACK);
-                    });
+                        mediaPlayer.stop();
+                        finalMediaPlayer.play();
+                        try{
+                            Thread.sleep(2000);
+                        }catch (InterruptedException ie){
+                            ie.printStackTrace();
+                        }
+                        flaga = false;
+                    }
+                };
+                runnable.start();
 
-                    //TODO: odtworzyc dzwiek beep.mp3, problem z odtworzeniem calego
+                //wlaczenie rotatetransition
+                rt1.play();
 
-                }
-                else
-                {
-                    phong7.setDiffuseColor(Color.LIMEGREEN);
-                    button.setMaterial(phong7);
-                    flaga = false;
-                    rt1.pause();
-                }
+
+                //TODO: odtworzyc dzwiek beep.mp3, problem z odtworzeniem calego
             }
-
             mouseOldY = me.getSceneY();
             mouseOldX = me.getSceneX();
+            //}
 
-        });
-
-        //stare obracanie z rubikscube
-        scene.setOnMouseDragged(me -> {
-            mousePosX = me.getSceneX();
-            mousePosY = me.getSceneY();
-            rotateX.setAngle(rotateX.getAngle()-(mousePosY - mouseOldY));
-            rotateY.setAngle(rotateY.getAngle()+(mousePosX - mouseOldX));
-            mouseOldX = mousePosX;
-            mouseOldY = mousePosY;
         });
 
         //press W nakreca pokretlo, press Q odkreca (o 10 stopni)
-        scene.setOnKeyPressed(event ->{
+        scene.setOnMouseDragged(me -> {
+            mousePosX = me.getSceneX();
+            mousePosY = me.getSceneY();
+            rotateX.setAngle(rotateX.getAngle() - (mousePosY - mouseOldY));
+            rotateY.setAngle(rotateY.getAngle() + (mousePosX - mouseOldX));
+            mouseOldX = mousePosX;
+            mouseOldY = mousePosY;
+        });
+        scene.setOnKeyPressed(event -> {
                     KeyCode keyCode = event.getCode();
 
-                    if(keyCode.equals(KeyCode.W))
-                    {
-                        Rotate rt0 = new Rotate(10, 0, 0, 0, Rotate.Y_AXIS);
-                        cylinder.getTransforms().add(rt0);
-                        przekretlo_angle = przekretlo_angle + 1;
-                    }
-                    if(keyCode.equals(KeyCode.Q))
-                    {
-                        Rotate rt0 = new Rotate(-10, 0, 0, 0, Rotate.Y_AXIS);
-                        cylinder.getTransforms().add(rt0);
-                        przekretlo_angle = przekretlo_angle - 1;
-                    }
-                    if (keyCode.equals(KeyCode.E))
-                    {
+                    if (keyCode.equals(KeyCode.W) && !flaga) {
+                        if(przekretlo_angle<200) {
+                            Rotate rt0 = new Rotate(10, 0, 0, 0, Rotate.Y_AXIS);
+                            cylinder.getTransforms().add(rt0);
+                            przekretlo_angle = przekretlo_angle + 1;
+                            text.setText(print(przekretlo_angle));
+                        }
 
-                        RotateTransition rt0 = new RotateTransition(Duration.millis(1000),drzwiczki);
-                        rt0.setAxis(new Point3D(0,1,0));
-                        rt0.setByAngle(120);
-                        drzwiczki.setRotationAxis(new Point3D(0,-1000,0));
-                        rt0.play();
+                    }
+                    if (keyCode.equals(KeyCode.Q) && !flaga) {
+                        if (przekretlo_angle > 0) {
+                            Rotate rt0 = new Rotate(-10, 0, 0, 0, Rotate.Y_AXIS);
+                            cylinder.getTransforms().add(rt0);
+                            przekretlo_angle = przekretlo_angle - 1;
+                            text.setText(print(przekretlo_angle));
+                        }
+                    }
+                    if (keyCode.equals(KeyCode.E)) {
+                        Media a = new Media(new File("C:\\Users\\Andrzej\\IdeaProjects\\Projekt_Kck2\\src\\door.mp3").toURI().toString());
+                        MediaPlayer doer = new MediaPlayer(a);
+
+                        if(!flaga) {
+                            if (!gotowe) {
+                                doer.setStartTime(new Duration(500));
+                                doer.setStopTime(new Duration(1400));
+                                doer.play();
+                                Rotate x = new Rotate(90, -400, -10, 0);
+                                x.axisProperty().setValue(Rotate.Y_AXIS);
+                                drzwiczki.getTransforms().add(x);
+                                gotowe = true;
+                            } else {
+                                doer.setStartTime(new Duration(1400));
+                                doer.setStopTime(new Duration(1900));
+                                doer.play();
+                                Rotate x = new Rotate(-90, -400, -10, 0);
+                                x.axisProperty().setValue(Rotate.Y_AXIS);
+                                drzwiczki.getTransforms().add(x);
+                                gotowe = false;
+                            }
+                        }
+
                     }
                 }
         );
 
-
-
-
-
         primaryStage.setTitle("KcK");
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+    public String print(int time){
+        Integer minuty=time/60, sekundy=time%60;
+        String z=" "+minuty.toString()+":";
+        if(sekundy==0){
+            z=z+"0";
+        }
+        else if(sekundy<10){
+            z=z+"0";
+        }
+        z=z+sekundy;
+        return z+" ";
     }
 
     public static void main(String[] args) {
